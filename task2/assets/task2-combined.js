44009c252fd163ee1140e1d07e5a24e53d0c9bc3
Fawad Ali <m.fawaadali98@gmail.com>
2019-10-21 09:24:03 +0500

    Task File

--- task2/assets/task2-combined.js
+++ task2/assets/task2-combined.js
@@ -140,22 +140,6 @@ function showData(type) {
     });
 }
 
-//  Dictionary with array object for each row
-function csvToJSON(csv) {
-    var lines = csv.split("\n");
-    var result = [];
-    var headers = lines[0].split(",");
-    for (var i = 1; i < lines.length - 1; i++) {
-        var obj = {};
-        var currentline = lines[i].split(",");
-        for (var j = 0; j < headers.length; j++) {
-            obj[headers[j]] = currentline[j];
-        }
-        result.push(obj);
-    }
-    return result;
-}
-
 //  Dictionary with object with array under each header
 // function csvToJSON(csvString){
 //   var allLines = csvString.split('\n');
